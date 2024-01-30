1944557a53d68211d6db2a308152f64b2484c243
blacklistedx <marchesejake@outlook.com>
2024-01-30 11:42:24 +1100

    Repo Structure

--- sub_ext/bing.py
+++ sub_ext/bing.py
@@ -22,21 +22,21 @@ class BurpExtender(IBurpExtender, IContextMenuFactory):
         callbacks.registerContextMenuFactory(self)
         return
 
-    def createMenuItems(self, context_menu):
+    def createMenuItems(self, context_menu):        #Required function (createMenuItems) when using 'IContextMenuFactory'
         self.context = context_menu
         menu_list = ArrayList()
-        menu_list.add(JMenuItem('Send to Bing', actionPerformed=self.bing_menu))
+        menu_list.add(JMenuItem('Send to Bing', actionPerformed=self.bing_menu))    #Name that pops up on request (calls bing menu function)
         return menu_list
 
-    def bing_menu(self, event):
+    def bing_menu(self, event):                                     #Built in functions in ContextMenu
         http_traffic = self.context.getSelectedMessages()
         print('%d requests highlighted' % len(http_traffic))
 
         for traffic in http_traffic:
-            http_service = traffic.getHttpService()
+            http_service = traffic.getHttpService()                 #assigning to service variable
             host = http_service.getHost()
-            print('User selected host: %s' % host)
-            self.bing_search(host)
+            print('User selected host: %s' % host)                  
+            self.bing_search(host)                                  #Passes host to search
         return
 
 
@@ -59,3 +59,38 @@ class BurpExtender(IBurpExtender, IContextMenuFactory):
 
     def bing_query(self, bing_query_string):
         print('Performing Bing Search: %s' % bing_query_string)
+        http_request = 'GET https://%s/v7.0/search?' % API_HOST
+        http_request += 'q=%s HTTP/1.1\r\n' % urllib.quote(bing_query_string)
+        http_request += 'Host: %s' % API_HOST
+        http_request += 'Connection:close\r\n'
+        http_request += 'Ocp-Apim-Subscription-Key: %s\r\n' % API_KEY
+        http_request += 'User-Agent: blacklistedx\r\n\r\n'
+
+        json_body = self._callbacks.makeHttpRequest(API_HOST, 443, True,
+                                                    http_request).tostring()
+
+        json_body = json_body.split('\r\n\r\n', 1)[1]
+        try:
+            response = json.loads(json_body)
+        except (TypeError, ValueError) as err:
+            print('No results from bing: %s' % err)
+        else:
+            sites = list()
+            if response.get('webPages'):
+                sites = response['webPages']['value']
+            if len(sites):
+                for site in sites:
+                    print('*'*100)
+                    print('Name: %s     ' % site['name'])
+                    print('URL: %s      ' % site['url'])
+                    print('Description: %r' $ site['snippet'])
+                    print('*'*100)
+
+                    java_url = URL(site['url'])
+                    if not self._callbacks.isInScope(java_url):
+                        print('Adding %s to Burp scope' % site['url'])
+                        self._callbacks.includeInScope(java_url)
+        else:
+            print('Empty response from Bing: %s' % bing_query_string)
+    return
+
